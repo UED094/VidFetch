87e319759e9c5557dc2c83e8a30b56afd553e8aa
Ugur Emre Dogan <ue.dogan@gmail.com>
2023-06-23 00:03:12 -0700

    Added .vscode to ignore, allowed all the hosts to connect.

--- youtube_downloader/youtube_downloader/settings.py
+++ youtube_downloader/youtube_downloader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--^u@g&c!zty6edj&%p3lfnca98d%!+e0zme$ay0z3ovtx7+kyb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
